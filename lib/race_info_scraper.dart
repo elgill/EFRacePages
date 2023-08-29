1fadb105d7bb86598156682b38350729ede5f9ec
Chris Gillin <cgillin2@gmail.com>
2023-08-28 21:55:21 -0400

    Add featured star marker

--- lib/race_info_scraper.dart
+++ lib/race_info_scraper.dart
@@ -9,10 +9,30 @@ Future<List<Race>> fetchRaces() async {
       var document = parse(response.body);
       List<Race> races = [];
 
+/*
       for (var section in document.querySelectorAll('section.post')) {
         var date = section.querySelector('.BlueDateDiv')?.text ?? section.querySelector('.RedDateDiv')?.text;
         var location = section.querySelector('div[style="text-align:center"] h3')?.text.trim();
         var name = section.querySelector('h3[style="text-align:center;font-size: 1.2em"]')?.text.trim();
+        var id = section.querySelector('a[href^="../race-results.asp?ID="]')?.attributes['href']?.split('=').last;
+
+        if (id != null && date != null && location != null && name != null) {
+          races.add(Race(id: id, date: date, location: location, name: name));
+        }
+      }
+*/
+
+      for (var section in document.querySelectorAll('section.post')) {
+        var isRedDate = section.querySelector('.RedDateDiv') != null;
+        var date = section.querySelector('.BlueDateDiv')?.text ?? section.querySelector('.RedDateDiv')?.text;
+        var location = section.querySelector('div[style="text-align:center"] h3')?.text.trim();
+        var name = section.querySelector('h3[style="text-align:center;font-size: 1.2em"]')?.text.trim();
+
+        // Add a star to the beginning of the name if it's from the red div
+        if (isRedDate && name != null) {
+          name = "★ $name";
+        }
+
         var id = section.querySelector('a[href^="../race-results.asp?ID="]')?.attributes['href']?.split('=').last;
 
         if (id != null && date != null && location != null && name != null) {
