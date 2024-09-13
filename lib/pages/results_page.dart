e7fb61d97244af3609fb2a372f220ffe9891aa27
Chris Gillin <cgillin2@gmail.com>
2024-09-13 09:26:49 -0400

    New results page

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -3,5 +3,5 @@ import 'package:ef_race_pages/base_web_view_page.dart';
 
 class ResultsPage extends BaseWebViewPage {
   const ResultsPage({Key? key, required String raceId})
-      : super(key: key, initialUrl: "https://www.elitefeats.com/race-results.asp?ID=$raceId");
+      : super(key: key, initialUrl: "https://www.elitefeats.com/race-results?ID=$raceId");
 }
